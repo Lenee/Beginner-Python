b4fa86f9dd294179dfa7e849e7834fe883b8a9ce
Lenee <Lenee.Dedeaux@gmail.com>
2015-06-14 13:16:23 -0500

    fourth commit

---
+++ Miles_per_gallon.py
@@ -0,0 +1,6 @@
+#This program is to calculate miles per gallon
+
+miles=float(input('Please enter the miles traveled: '))
+gallons=float(input('Please enter how many gallons of gas used: '))
+mpg=miles/gallons
+print('Your MPG is: ', format(mpg,'.2f'),'!')
