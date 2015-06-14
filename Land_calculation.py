cb0032280a60298aec67aa325d856b807a8a231f
Lenee <Lenee.Dedeaux@gmail.com>
2015-06-14 13:15:23 -0500

    third commit

---
+++ Land_calculation.py
@@ -0,0 +1,6 @@
+#!\Users\Lenee\Documents\Python_Lab_docs\Exercises_Book\Chapter2\Land_calculation.py
+acre=43560
+total_square_feet=float(input('Enter the total square feet you want converted\
+to acres: '))
+converted_acres=total_square_feet/acre
+print('That converts to ',converted_acres,' acres.')
