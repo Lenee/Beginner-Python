6273764e6b2c5b626a8f8c1b48e2ed369e5774a1
Lenee <Lenee.Dedeaux@gmail.com>
2015-06-14 11:03:50 -0500

    first commit

---
+++ Distance_traveled.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+speed=60
+time=1
+distance=speed*time
+print('Travel 5 hours at 60 miles per hour = ',5*distance,' miles.')
+print('Travel 8 hours at 60 miles per hour = ', 8*distance,' miles.')
+print('Travel 12 hours at 60 miles per hour = ', 12*distance,' miles.')
